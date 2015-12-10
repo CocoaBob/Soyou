bb3e81f987ab3c5831dc2e4aba968a02a12595ba
jiyuny <jiyuny@yahoo.fr>
2015-12-11 00:23:25 +0100

    add brandsViewController

--- iPrices/Sources/Modules/Brands/BrandsViewController.swift
+++ iPrices/Sources/Modules/Brands/BrandsViewController.swift
@@ -9,6 +9,10 @@
 class BrandsViewController: BaseViewController/*, UICollectionViewDelegate, UICollectionViewDataSource, CHTCollectionViewDelegateWaterfallLayout*/ {
     
     @IBOutlet var _collectionView: UICollectionView?
+    var isEdgeSwiping: Bool = false
+    
+    // TODO to be replaced
+    var brands = [["id": 1,"label": "BURBERRY","imageUrl": "http://www.geocities.ws/iprice/imgs/o-burberry.jpg","extra": "","type": "brand"], ["id": 2,"label": "BALENCIAGA","imageUrl": "http://www.geocities.ws/iprice/imgs/o-balenciaga.jpg","extra": "","type": "brand"]]
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -24,5 +28,118 @@ class BrandsViewController: BaseViewController/*, UICollectionViewDelegate, UICo
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // UINavigationController delegate
+        self.navigationController?.delegate = self;
+        self.navigationController?.interactivePopGestureRecognizer?.delegate = self;
+        
+        // Data
+        requestBrandsList(nil)
+    }
+}
+
+// MARK: Data
+extension BrandsViewController {
+    private func handleSuccess(responseObject: AnyObject?, _ relativeID: NSNumber?) {
+        
+    }
+    
+    private func handleError(error: NSError?) {
+        print("\(error)")
+    }
+    
+    func requestBrandsList(relativeID: NSNumber?) {
+        
+    }
+}
+
+// MARK: - CollectionView Delegate Methods
+extension BrandsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UIScrollViewDelegate {
+
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.brands.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: BrandCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("BrandCollectionViewCell", forIndexPath: indexPath) as! BrandCollectionViewCell
+        
+        let brand = self.brands[indexPath.row]
+        
+        cell.tltTextView?.clipsToBounds = true
+        cell.tltTextView?.layer.shadowRadius = 1
+        cell.tltTextView?.layer.shadowColor = UIColor.blackColor().CGColor
+        cell.tltTextView?.layer.shadowOpacity = 1
+        cell.tltTextView?.layer.shadowOffset = CGSizeZero
+        cell.tltTextView?.text = brand["label"] as! String
+
+        if let imageURLString = brand["imageUrl"], let imageURL = NSURL(string: imageURLString as! String) {
+            cell.fgImageView?.sd_setImageWithURL(imageURL, placeholderImage: UIImage(named: "iTunesArtwork"), completed: { (image: UIImage!, error: NSError!, type: SDImageCacheType, url: NSURL!) -> Void in
+                collectionView.reloadItemsAtIndexPaths([indexPath])
+            })
+        }
+
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        self.isEdgeSwiping = false
+    }
+}
+
+// MARK: UIGestureRecognizerDelegate (interactivePopGestureRecognizer.delegate)
+extension BrandsViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
+        self.isEdgeSwiping = true
+        return true
+    }
+}
+
+// MARK: - UINavigationControllerDelegate
+extension BrandsViewController: UINavigationControllerDelegate {
+    
+    func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        if self.isEdgeSwiping {
+            self.isEdgeSwiping = false
+            return nil
+        }
+        
+        if fromVC is RMPZoomTransitionAnimating && toVC is RMPZoomTransitionAnimating {
+            let src = fromVC as! protocol<RMPZoomTransitionAnimating, RMPZoomTransitionDelegate>
+            let dest = toVC as! protocol<RMPZoomTransitionAnimating, RMPZoomTransitionDelegate>
+            
+            // If one of the frames is invisible
+            if (operation == .Pop &&
+                src.transitionDestinationImageViewFrame().size.height == 0) {
+                    return nil
+            }
+            
+            let animator = RMPZoomTransitionAnimator()
+            animator.goingForward = (operation == .Push)
+            animator.sourceTransition = src
+            animator.destinationTransition = dest
+            return animator
+        }
+        
+        return nil
+    }
+}
+
+// MARK: - Custom cells
+class BrandCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var fgImageView: UIImageView!
+    @IBOutlet var tltTextView: UITextView!
+    
+    override func prepareForReuse() {
+        tltTextView?.text = nil
+        tltTextView?.attributedText = nil
     }
 }
