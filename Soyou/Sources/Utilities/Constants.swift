1ba2300c05504b9cc7d6552c457a7245f1995ade
CocoaBob <mengke.wang@gmail.com>
2016-04-03 12:23:44 +0200

    Changd data updating interval to 15 minutes. Added gradient text background to brand cell.

--- Soyou/Sources/Utilities/Constants.swift
+++ Soyou/Sources/Utilities/Constants.swift
@@ -43,7 +43,7 @@ public struct Cons {
         static let lastInstalledBuild                       = "lastInstalledBuild"                      // Stored in NSUserDefaults
         static let userCurrency                             = "userCurrency"                            // Stored in NSUserDefaults
         static let lastUpdateDate                           = "lastUpdateDate"                          // Stored in NSUserDefaults
-        static let updateInterval                           = 86400.0                                   // 1 day
+        static let updateInterval                           = 900.0                                     // 15 minutes
     }
     
     struct DB {
