e005ec3558413d397eb8d6a9d2742d2cb5653491
jiyun <dev@soyou.io>
2017-11-30 22:17:52 +0100

    Updated api doc

--- API/api_project.js
+++ API/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-11-27T18:23:14.704Z",
+    "time": "2017-11-30T21:16:31.284Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
