2fc8141176e1ce13f87a94fc7b6e04008cfcc607
jiyun <dev@soyou.io>
2018-01-02 20:27:11 +0100

    added secure in friends and circle urls

--- API/api_project.js
+++ API/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-01-02T00:05:15.472Z",
+    "time": "2018-01-02T19:26:18.975Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
