e4b1f2a0806852767c869ee1f5c75b306a12f484
jiyun <dev@soyou.io>
2018-02-06 06:51:48 +0100

    added badges to following/followers and user search APIs

--- API/api_project.js
+++ API/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-02-05T22:33:56.489Z",
+    "time": "2018-02-06T05:50:55.778Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
