9a1922505841d27ff2774595d871d7c3d7ec6f1b
Jiyun <dev@soyou.io>
2018-05-20 23:36:32 +0200

    Added doc for crawl

--- API/api_project.js
+++ API/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-05-14T06:48:51.396Z",
+    "time": "2018-05-20T21:35:31.328Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
